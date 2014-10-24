e81508a089d32a542aa0465a9d74bd66965bcfac
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-10-24 21:46:47 +0800

    完成插入整數的按鈕

--- SignalListApplet.java
+++ SignalListApplet.java
@@ -6,6 +6,7 @@ import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import java.awt.TextArea;
 import javax.swing.JFrame;
+import java.awt.event.*;
 
 public class SignalListApplet extends JApplet {
 	private static JTextField textField; // Let user input a integer to insert at head.
@@ -20,6 +21,7 @@ public class SignalListApplet extends JApplet {
 	JButton btnNewButton_6 = new JButton("\u78BA\u5B9A\r\n"); // click it can search a integer which user input in textField_2, and display the result in textArea_1.
 	static JTextArea textArea; // display all the action that click any button.
 	static TextArea textArea_1; // display the search result when you click btnNewButton_6.
+	List< Integer > list = new List< Integer >(); // create a List
 	
 		// In order to run standalone as a standalone application, we add the main method.
 	public static void main(String[] args) {
@@ -120,10 +122,68 @@ public class SignalListApplet extends JApplet {
 		getContentPane().add(scrollPane);
 		
 		textArea_1 = new TextArea();
-		textArea_1.setEnabled(false);
+		
 		textArea_1.setEditable(false);
 		textArea_1.setBounds(10, 172, 440, 227);
 		getContentPane().add(textArea_1);
 
+		btnNewButton.addActionListener(new ButtonListener()); // Register listener.
+		btnNewButton_1.addActionListener(new ButtonListener()); // Register listener.
+		btnNewButton_2.addActionListener(new ButtonListener()); // Register listener.
+		btnNewButton_3.addActionListener(new ButtonListener()); // Register listener.
+		btnNewButton_4.addActionListener(new ButtonListener()); // Register listener.
+		btnNewButton_5.addActionListener(new ButtonListener()); // Register listener.
+		btnNewButton_6.addActionListener(new ButtonListener()); // Register listener.
+
 	}
+
+	// A class is responsible for action presentation.
+	private class ButtonListener implements ActionListener{
+		
+		
+		@Override
+		public void actionPerformed(ActionEvent e){
+			if(e.getSource() == btnNewButton){
+				if(textField.getText().compareTo("") != 0){
+					StringBuilder op = new StringBuilder();
+					op.append(textArea_1.getText() + "\n");
+					int number1 = Integer.parseInt(textField.getText());
+    				list.insertAtFront(number1);
+    				op.append("You insert a integer " + number1 + " at head.\n" + list.print());
+    				String op1 = new String(op);
+    				textArea_1.setText(op1);
+    				textField.setText("");
+    			}else{
+
+    			}
+			}else if(e.getSource() == btnNewButton_1){
+				if(textField_1.getText().compareTo("") != 0){
+					StringBuilder op = new StringBuilder();
+					op.append(textArea_1.getText() + "\n");
+					int number2 = Integer.parseInt(textField_1.getText());
+    				list.insertAtBack(number2);
+    				op.append("You insert a integer " + number2 + " a tail.\n" + list.print());
+    				String op1 = new String(op);
+    				textArea_1.setText(op1);
+    				textField_1.setText("");
+        		}else{
+
+    			}
+			}else if(e.getSource() == btnNewButton_2){
+				textArea_1.setText("btnNewButton_2");
+			}else if(e.getSource() == btnNewButton_3){
+				textArea_1.setText("btnNewButton_3");
+			}else if(e.getSource() == btnNewButton_4){
+				textArea_1.setText("btnNewButton_4");
+			}else if(e.getSource() == btnNewButton_5){
+				textArea_1.setText("btnNewButton_5");
+			}else if(e.getSource() == btnNewButton_6){
+				textArea_1.setText("btnNewButton_6");
+			}else{
+				textArea_1.setText("error!!");
+			}
+			
+		}// end method actionPerformed
+	}// end inner class ButtonListener
 }
+
