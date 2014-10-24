ec24948f504d4e325d15eb0fdcf016857eb0e994
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-10-24 21:49:29 +0800

    Create EmptyListException.java

---
+++ EmptyListException.java
@@ -0,0 +1,26 @@
+public class EmptyListException extends RuntimeException 
+{
+   int status = 0;
+   // no-argument constructor
+   public EmptyListException()
+   {
+      this( "List" ); // call other EmptyListException constructor
+   } // end EmptyListException no-argument constructor
+
+   // one-argument constructor
+   public int EmptyListException( String name )
+   {
+      setStatus(0);
+      
+   } // end EmptyListException one-argument constructor
+
+   public void setStatus(int newStatus){
+      status = newStatus;
+   }
+
+   public int getStatus(){
+      return status;
+   }
+} // end class EmptyListException
+
+
